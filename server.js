0cc1ab29934dcce1a6af2b96ae6c899eb0fed5ed
Zeeshan Khan <zkhan1093@gmail.com>
2019-07-23 23:48:51 +0530

    adding server

---
+++ server.js
@@ -0,0 +1,11 @@
+// server.js
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+app = express()
+app.use(serveStatic(__dirname + "/dist"))
+var port = process.env.PORT || 8080
+app.listen(port)
+
+//eslint-disable-next-line
+console.log('server started '+ port)
