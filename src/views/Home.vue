d7c0e4360894f4e1d6eadff13820aa98ff912e5f
Zeeshan Khan <zkhan1093@gmail.com>
2020-09-22 20:02:01 +0530

    fix favicon, using image from cloudinary

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,7 @@
           <div class="q-pr-md row no-wrap">
             <q-card
               class="my-card q-ma-md"
-              style="width: 250px;"
+              style="width: 230px;"
               v-for="item in cat.items"
               :key="item.id"
               @click.prevent="detail_page(item)"
@@ -57,27 +57,56 @@ export default {
         {
           name: "Today's Releases",
           items: [
-            { id: 1, image: "https://cdn.quasar.dev/img/mountains.jpg" },
-            { id: 2, image: "https://cdn.quasar.dev/img/mountains.jpg" },
-            { id: 3, image: "https://cdn.quasar.dev/img/mountains.jpg" },
-            { id: 4, image: "https://cdn.quasar.dev/img/mountains.jpg" },
-            { id: 5, image: "https://cdn.quasar.dev/img/mountains.jpg" },
-            { id: 6, image: "https://cdn.quasar.dev/img/mountains.jpg" },
-            { id: 7, image: "https://cdn.quasar.dev/img/mountains.jpg" },
-            { id: 8, image: "https://cdn.quasar.dev/img/mountains.jpg" },
+            {
+              id: 1,
+              image:
+                "https://res.cloudinary.com/moviepedia/image/upload/w_380,h_270/v1600784051/movie_thumbs/movie_15_e6i6th.jpg",
+            },
+            {
+              id: 2,
+              image:
+                "https://res.cloudinary.com/moviepedia/image/upload/w_380,h_270/v1600784051/movie_thumbs/movie_14_um32ao.jpg",
+            },
+            {
+              id: 3,
+              image:
+                "https://res.cloudinary.com/moviepedia/image/upload/w_380,h_270/v1600784051/movie_thumbs/movie_13_ripwxl.jpg",
+            },
+            {
+              id: 4,
+              image:
+                "https://res.cloudinary.com/moviepedia/image/upload/w_380,h_270/v1600784051/movie_thumbs/movie_10_kpupgd.jpg",
+            },
+            {
+              id: 5,
+              image:
+                "https://res.cloudinary.com/moviepedia/image/upload/w_380,h_270/v1600784050/movie_thumbs/movie_11_gzerr3.jpg",
+            },
           ],
         },
         {
           name: "Star Pick",
           items: [
-            { id: 1, image: "https://cdn.quasar.dev/img/mountains.jpg" },
-            { id: 2, image: "https://cdn.quasar.dev/img/mountains.jpg" },
-            { id: 3, image: "https://cdn.quasar.dev/img/mountains.jpg" },
-            { id: 4, image: "https://cdn.quasar.dev/img/mountains.jpg" },
-            { id: 5, image: "https://cdn.quasar.dev/img/mountains.jpg" },
-            { id: 6, image: "https://cdn.quasar.dev/img/mountains.jpg" },
-            { id: 7, image: "https://cdn.quasar.dev/img/mountains.jpg" },
-            { id: 8, image: "https://cdn.quasar.dev/img/mountains.jpg" },
+            {
+              id: 1,
+              image:
+                "https://res.cloudinary.com/moviepedia/image/upload/w_380,h_270/v1600784098/judges_thumbs/20200922_182634_0000_xd8vfn.png",
+            },
+            {
+              id: 2,
+              image:
+                "https://res.cloudinary.com/moviepedia/image/upload/w_380,h_270/v1600784097/judges_thumbs/20200922_182607_0000_dwlpn2.png",
+            },
+            {
+              id: 3,
+              image:
+                "https://res.cloudinary.com/moviepedia/image/upload/w_380,h_270/v1600784095/judges_thumbs/20200922_182642_0000_ispwzr.png",
+            },
+            {
+              id: 4,
+              image:
+                "https://res.cloudinary.com/moviepedia/image/upload/w_380,h_270/v1600784095/judges_thumbs/20200922_182626_0000_osijza.png",
+            },
           ],
         },
         {
