ad6052714749fd0fdd529af576cd21e8fda538c1
Zeeshan Khan <zkhan1093@gmail.com>
2020-10-03 02:19:47 +0530

    script final

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+WORKSPACE=/home/zeeshan/moviepediafilms.com
+USER=zeeshan
+DEST_DIR=/var/www/moviepediafilms.com
 
-cd /home/zeeshan/moviepediafilms.com && \
-/usr/bin/git pull && \
-/home/zeeshan/.nvm/versions/node/v14.11.0/bin/npm install --only=prod && \
-/home/zeeshan/.nvm/versions/node/v14.11.0/bin/npm run build && \
-cp -r /home/zeeshan/moviepediafilms.com/dist/* /var/www/moviepediafilms.com/
+npm=/home/zeeshan/.nvm/versions/node/v14.11.0/bin/npm
+git=/usr/bin/git
+
+su -c "cd $WORKSPACE &&
+$git pull &&
+$npm install &&
+$npm run build" -m "$USER" &&\
+rm -rf $DEST_DIR/* &&\
+cp -r $WORKSPACE/dist/* $DEST_DIR/
