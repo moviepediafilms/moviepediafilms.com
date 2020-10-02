3122786fc2b9f533bae3d77f7f18f8cc6b84c81b
Zeeshan Khan <zkhan1093@gmail.com>
2020-10-03 01:35:20 +0530

    fix typo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ cd /home/zeeshan/moviepediafilms.com && \
 /usr/bin/git pull && \
 /home/zeeshan/.nvm/versions/node/v14.11.0/bin/npm install --only=prod && \
 /home/zeeshan/.nvm/versions/node/v14.11.0/bin/npm run build && \
-cp -r /home/zeeshan/moviepediafilms.com/dist/* /var/www/html/moviepediafilms.com/
+cp -r /home/zeeshan/moviepediafilms.com/dist/* /var/www/moviepediafilms.com/
