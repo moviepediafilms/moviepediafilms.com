700df69e44a649d52096998ad0f280a4c56b0b78
Zeeshan Khan <zkhan1093@gmail.com>
2020-10-04 12:26:21 +0530

    removing local tar

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,8 +16,8 @@ $git checkout master
 $git pull
 $npm install --only=prod
 $npm run build
-rm $WORKSPACE/$TAR_FILE
 cd $WORKSPACE/dist
 $tar -zcvf $WORKSPACE/$TAR_FILE .
 scp $WORKSPACE/$TAR_FILE $USER@$SERVER:$TMP_FOLDER/
 ssh $USER@$SERVER "$tar -zxvf $TMP_FOLDER/$TAR_FILE -C $DEST_DIR && rm $TMP_FOLDER/$TAR_FILE"
+rm $WORKSPACE/$TAR_FILE
