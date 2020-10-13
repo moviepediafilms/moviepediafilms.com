7d965a0bf27a0e3a5cea17c8a7c3d2177007a63b
Zeeshan Khan <zkhan1093@gmail.com>
2020-10-13 12:43:10 +0530

    fix #29 list items

--- src/views/PrivacyPolicy.vue
+++ src/views/PrivacyPolicy.vue
@@ -1,177 +1,186 @@
 <template>
   <base-layout>
     <div class="q-ma-md q-pt-lg">
-      <h4 class="text-primary mt-5 text-center text-uppercase">
-        Moviepedia Privacy Policy
-      </h4>
-      <div class="text-h5 q-mt-lg q-mb-sm text-primary">
-        Moviepedia Privacy Policy
-      </div>
-      <p>
-        At Moviepedia, accessible from
-        <a
-          href="https://www.moviepediafilms.com"
-          class="text-primary text-decoration-none"
-          >https://www.moviepediafilms.com</a
-        >, one of our main priorities is the privacy of our visitors. This
-        Privacy Policy document contains types of information that is collected
-        and recorded by MoviepediaFilms.com and how we use it. If you have
-        additional questions or require more information about our Privacy
-        Policy, feel free to contact us.
-      </p>
-      <p>
-        This Privacy Policy applies only to our online activities and is valid
-        for visitors to our website with regards to the information that they
-        shared and/or collect in MoviepediaFilms.com. This policy is not
-        applicable to any information collected offline or via channels other
-        than this website.
-      </p>
-      <p>
-        <strong>Consent</strong> By using our website, you hereby consent to our
-        Privacy Policy and agree to its terms.
-      </p>
-      <div class="text-h5 q-mt-lg q-mb-sm text-primary">
-        Information we collect
-      </div>
-      <p>
-        The personal information that you are asked to provide, and the reasons
-        why you are asked to provide it, Is stated below:-
-      </p>
-      <p>
-        When you register for an Account, we may ask for your contact
-        information, including items such as name, Skill, email address, City,
-        Gender, DOB telephone number. When submitting a film information such as
-        the name and email address of the filmmaker is also asked. Apart from
-        this, we collect information regarding payment methods to facilitate
-        online transactions for film submission.
-      </p>
-      <p>
-        How we use your information We use the information we collect in various
-        ways, including to:
-      </p>
-      <p>
-        • Provide, operate, and maintain our website • Improve, personalize, and
-        expand our website • Understand and analyze how you use our website •
-        Develop new products, services, features, and functionality •
-        Communicate with you, either directly or through one of our partners,
-        including for customer service, to provide you with updates and other
-        information relating to the website, and for marketing and promotional
-        purposes • Send you emails • Find and prevent fraud • Log Files
-      </p>
-      <p>
-        Moviepedia follows a standard procedure of using log files. These files
-        log visitors when they visit websites. All hosting companies do this and
-        a part of hosting services&#39; analytics. The information collected by
-        log files include internet protocol (IP) addresses, browser type,
-        Internet Service Provider (ISP), date and time stamp, referring/exit
-        pages, and possibly the number of clicks. These are not linked to any
-        information that is personally identifiable. The purpose of the
-        information is for analyzing trends, administering the site, tracking
-        users&#39; movement on the website, and gathering demographic
-        information.
-      </p>
-      <div class="text-h5 q-mt-lg q-mb-sm text-primary">
-        Cookies and Web Beacons
-      </div>
-      <div class="text-h5 q-mt-lg q-mb-sm text-primary">What are Cookies?</div>
-      <p>
-        Cookies are text files with small pieces of data — like a username and
-        password — that are used to identify your computer as you use a computer
-        network. Specific cookies known as HTTP cookies are used to identify
-        specific users and improve your web browsing experience.
-      </p>
-      <p>
-        MoviepediaFilms.com uses &#39;cookies&#39;. These cookies are used to
-        store information including visitors&#39; preferences, and the pages on
-        the website that the visitor accessed or visited. The information is
-        used to optimize the users&#39; experience by customizing our web page
-        content based on visitors&#39; browser type and/or other information..
-      </p>
-      <div class="text-h5 q-mt-lg q-mb-sm text-primary">
-        Advertising Partners Privacy Policies
-      </div>
-      <p>
-        You may consult this list to find the Privacy Policy for each of the
-        advertising partners of MoviepediaFilms.com.
-      </p>
-      <p>
-        Presently, there is no such advertising partner for MoviepediaFilms.com
-      </p>
-      <p>
-        Third-party ad servers or ad networks use technologies like cookies,
-        JavaScript, or Web Beacons that are used in their respective
-        advertisements and links that appear on moviepediafilms.com, which are
-        sent directly to users&#39; browsers. They automatically receive your IP
-        address when this occurs. These technologies are used to measure the
-        effectiveness of their advertising campaigns and/or to personalize the
-        advertising content that you see on websites that you visit.
-      </p>
-      <p>
-        Note: MoviepediaFilms.com has no access to or control over these cookies
-        that are used by third-party advertisers.
-      </p>
-      <div class="text-h5 q-mt-lg q-mb-sm text-primary">
-        Third-Party Privacy Policies
-      </div>
-      <p>
-        MoviepediaFilms.com&#39;s Privacy Policy does not apply to other
-        advertisers or websites. Thus, we are advising you to consult the
-        respective Privacy Policies of these third-party ad servers for more
-        detailed information. It may include their practices and instructions
-        about how to opt-out of certain options.
-      </p>
-      <p>
-        You can choose to disable cookies through your individual browser
-        options. To know more detailed information about cookie management with
-        specific web browsers, it can be found at the browsers&#39; respective
-        websites.
-      </p>
-      <div class="text-h5 q-mt-lg q-mb-sm text-primary">
-        Does Moviepedia sell your data?
-      </div>
-      <p>
-        We do not sell your personal information to anyone. We use the
-        information that we collect to customise our services for you, including
-        providing recommendations, personalising search results and profiling of
-        filmmakers. GDPR Data Protection Rights
-      </p>
-      <p>
-        We would like to make sure you are fully aware of all of your data
-        protection rights. Every user is entitled to the following: • The right
-        to access – You have the right to request copies of your personal data.
-        We may charge you a small fee for this service. • The right to
-        rectification – You have the right to request that we correct any
-        information you believe is inaccurate. You also have the right to
-        request that we complete the information you believe is incomplete. •
-        The right to erasure – You have the right to request that we erase your
-        personal data, under certain conditions. • The right to restrict
-        processing – You have the right to request that we restrict the
-        processing of your personal data, under certain conditions. • The right
-        to object to processing – You have the right to object to our processing
-        of your personal data, under certain conditions. • The right to data
-        portability – You have the right to request that we transfer the data
-        that we have collected to another organization, or directly to you,
-        under certain conditions. If you make a request, we have one month to
-        respond to you. If you would like to exercise any of these rights,
-        please contact us.
-      </p>
-      <div class="text-h5 q-mt-lg q-mb-sm text-primary">
-        Children&#39;s Information
-      </div>
-      <p>
-        Another part of our priority is adding protection for children while
-        using the internet. We encourage parents and guardians to observe,
-        participate in, and/or monitor and guide their online activity.
-      </p>
-      <p>
-        MoviepediaFilms.com does not knowingly collect any Personal Identifiable
-        Information from children under the age of 13. If you think that your
-        child provided this kind of information on our website, we strongly
-        encourage you to contact us immediately and we will do our best efforts
-        to promptly remove such information from our records.
-      </p>
+        <h4 class="text-primary mt-5 text-center text-uppercase">
+            Moviepedia Privacy Policy
+        </h4>
+        <div class="text-h5 q-mt-lg q-mb-sm text-primary">
+            Moviepedia Privacy Policy
+        </div>
+        <p>
+            At Moviepedia, accessible from
+            <a href="https://www.moviepediafilms.com"
+                class="text-primary text-decoration-none">https://www.moviepediafilms.com</a>, one of our main
+            priorities is the privacy of our visitors. This
+            Privacy Policy document contains types of information that is collected
+            and recorded by MoviepediaFilms.com and how we use it. If you have
+            additional questions or require more information about our Privacy
+            Policy, feel free to contact us.
+        </p>
+        <p>
+            This Privacy Policy applies only to our online activities and is valid
+            for visitors to our website with regards to the information that they
+            shared and/or collect in MoviepediaFilms.com. This policy is not
+            applicable to any information collected offline or via channels other
+            than this website.
+        </p>
+        <p>
+            <strong>Consent</strong> By using our website, you hereby consent to our
+            Privacy Policy and agree to its terms.
+        </p>
+        <div class="text-h5 q-mt-lg q-mb-sm text-primary">
+            Information we collect
+        </div>
+        <p>
+            The personal information that you are asked to provide, and the reasons
+            why you are asked to provide it, Is stated below:-
+        </p>
+        <p>
+            When you register for an Account, we may ask for your contact
+            information, including items such as name, Skill, email address, City,
+            Gender, DOB telephone number. When submitting a film information such as
+            the name and email address of the filmmaker is also asked. Apart from
+            this, we collect information regarding payment methods to facilitate
+            online transactions for film submission.
+        </p>
+        <p>
+            How we use your information We use the information we collect in various
+            ways, including to:
+        </p>
+        <p>
+        <ul style="list-style-type: disc; list-style-position: outside" class="q-ml-md">
+            <li>Provide, operate, and maintain our website </li>
+            <li>Improve, personalize, and expand our website </li>
+            <li>Understand and analyze how you use our website </li>
+            <li>Develop new products, services, features, and functionality </li>
+            <li>Communicate with you, either directly or through one of our partners,
+                including for customer service, to provide you with updates and other
+                information relating to the website, and for marketing and promotional
+                purposes </li>
+            <li>Send you emails </li>
+            <li>Find and prevent fraud </li>
+            <li>Log Files</li>
+        </ul>
+        </p>
+        <p>
+            Moviepedia follows a standard procedure of using log files. These files
+            log visitors when they visit websites. All hosting companies do this and
+            a part of hosting services&#39; analytics. The information collected by
+            log files include internet protocol (IP) addresses, browser type,
+            Internet Service Provider (ISP), date and time stamp, referring/exit
+            pages, and possibly the number of clicks. These are not linked to any
+            information that is personally identifiable. The purpose of the
+            information is for analyzing trends, administering the site, tracking
+            users&#39; movement on the website, and gathering demographic
+            information.
+        </p>
+        <div class="text-h5 q-mt-lg q-mb-sm text-primary">
+            Cookies and Web Beacons
+        </div>
+        <div class="text-h5 q-mt-lg q-mb-sm text-primary">What are Cookies?</div>
+        <p>
+            Cookies are text files with small pieces of data — like a username and
+            password — that are used to identify your computer as you use a computer
+            network. Specific cookies known as HTTP cookies are used to identify
+            specific users and improve your web browsing experience.
+        </p>
+        <p>
+            MoviepediaFilms.com uses &#39;cookies&#39;. These cookies are used to
+            store information including visitors&#39; preferences, and the pages on
+            the website that the visitor accessed or visited. The information is
+            used to optimize the users&#39; experience by customizing our web page
+            content based on visitors&#39; browser type and/or other information..
+        </p>
+        <div class="text-h5 q-mt-lg q-mb-sm text-primary">
+            Advertising Partners Privacy Policies
+        </div>
+        <p>
+            You may consult this list to find the Privacy Policy for each of the
+            advertising partners of MoviepediaFilms.com.
+        </p>
+        <p>
+            Presently, there is no such advertising partner for MoviepediaFilms.com
+        </p>
+        <p>
+            Third-party ad servers or ad networks use technologies like cookies,
+            JavaScript, or Web Beacons that are used in their respective
+            advertisements and links that appear on moviepediafilms.com, which are
+            sent directly to users&#39; browsers. They automatically receive your IP
+            address when this occurs. These technologies are used to measure the
+            effectiveness of their advertising campaigns and/or to personalize the
+            advertising content that you see on websites that you visit.
+        </p>
+        <p>
+            Note: MoviepediaFilms.com has no access to or control over these cookies
+            that are used by third-party advertisers.
+        </p>
+        <div class="text-h5 q-mt-lg q-mb-sm text-primary">
+            Third-Party Privacy Policies
+        </div>
+        <p>
+            MoviepediaFilms.com&#39;s Privacy Policy does not apply to other
+            advertisers or websites. Thus, we are advising you to consult the
+            respective Privacy Policies of these third-party ad servers for more
+            detailed information. It may include their practices and instructions
+            about how to opt-out of certain options.
+        </p>
+        <p>
+            You can choose to disable cookies through your individual browser
+            options. To know more detailed information about cookie management with
+            specific web browsers, it can be found at the browsers&#39; respective
+            websites.
+        </p>
+        <div class="text-h5 q-mt-lg q-mb-sm text-primary">
+            Does Moviepedia sell your data?
+        </div>
+        <p>
+            We do not sell your personal information to anyone. We use the
+            information that we collect to customise our services for you, including
+            providing recommendations, personalising search results and profiling of
+            filmmakers. GDPR Data Protection Rights
+        </p>
+        <p>
+            We would like to make sure you are fully aware of all of your data
+            protection rights. Every user is entitled to the following:
+        <ul style="list-style-type: disc; list-style-position: outside" class="q-ml-md">
+            <li>The right to access – You have the right to request copies of your personal data. We may charge you a
+                small fee for this service. </li>
+            <li>The right to rectification – You have the right to request that we
+                correct any information you believe is inaccurate. You also have the
+                right to request that we complete the information you believe is
+                incomplete. </li>
+            <li>The right to erasure – You have the right to request that we erase your
+                personal data, under certain conditions. </li>
+            <li>The right to restrict processing – You have the right to request that we
+                restrict the processing of your personal data, under certain conditions.
+            </li>
+            <li>The right to object to processing – You have the right to object to our
+                processing of your personal data, under certain conditions. </li>
+            <li>The right to data portability – You have the right to request that we
+                transfer the data that we have collected to another organization, or
+                directly to you, under certain conditions. If you make a request, we
+                have one month to respond to you. If you would like to exercise any of
+                these rights, please contact us.
+            </li>
+        </ul>
+        </p>
+        <div class="text-h5 q-mt-lg q-mb-sm text-primary">
+            Children&#39;s Information
+        </div>
+        <p>
+            Another part of our priority is adding protection for children while
+            using the internet. We encourage parents and guardians to observe,
+            participate in, and/or monitor and guide their online activity.
+        </p>
+        <p>
+            MoviepediaFilms.com does not knowingly collect any Personal Identifiable
+            Information from children under the age of 13. If you think that your
+            child provided this kind of information on our website, we strongly
+            encourage you to contact us immediately and we will do our best efforts
+            to promptly remove such information from our records.
+        </p>
     </div>
-  </base-layout>
+</base-layout>
 </template>
 <script>
 import BaseLayout from "@/layouts/Base";
