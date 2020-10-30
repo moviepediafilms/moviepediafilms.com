060e26621a9abf3a78fb34375157bce9459f06dc
Zeeshan Khan <zkhan1093@gmail.com>
2020-10-30 08:54:25 +0530

    added comp for diff profile types

---
+++ src/components/ProfilePicture.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="row justify-center">
+    <q-avatar
+      size="110px"
+      style="margin-left: 8px"
+      :class="{ 'bg-grey-9': show_dummy_img || !my_profile.image }"
+    >
+      <img
+        :src="my_profile.image"
+        @error="on_profile_img_load_err"
+        v-if="!show_dummy_img && my_profile.image"
+      />
+      <q-icon name="mdi-account" size="145px" color="grey-5" v-else />
+    </q-avatar>
+    <div class="self-end" style="margin-left: -24px">
+      <q-btn
+        round
+        flat
+        @click="on_change_icon"
+        size="8px"
+        color="white"
+        v-ripple
+        class="q-pa-xs bg-grey-7"
+        icon="mdi-camera"
+      />
+    </div>
+    <div style="display: none">
+      <q-file
+        ref="file_select"
+        v-model="profile_image.file"
+        accept=".jpg, image/*"
+        @rejected="on_profile_image_reject"
+      />
+    </div>
+    <q-dialog v-model="change_icon_dialog">
+      <q-card class="" style="width: 400px; max-width: 80vw">
+        <q-card-section class="text-center">
+          <div class="text-h6 q-mb-lg">Change Picture</div>
+          <div>
+            <vue-cropper
+              ref="cropper"
+              :aspect-ratio="1"
+              :src="profile_image_url"
+              alt="Profile Picture"
+            >
+            </vue-cropper>
+          </div>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat label="Cancel" v-close-popup />
+          <q-btn
+            flat
+            :loading="profile_image.loading"
+            :disable="!profile_image.file"
+            color="primary"
+            label="Upload"
+            @click="save_profile_image"
+          />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+<script>
+import VueCropper from "vue-cropperjs";
+import "cropperjs/dist/cropper.css";
+import { PROFILE_IMAGE_UPDATE } from "@/store/actions";
+export default {
+  components: {
+    VueCropper,
+  },
+  data() {
+    return {
+      show_dummy_img: false,
+      change_icon_dialog: false,
+      profile_image: {
+        file: null,
+        error: "",
+        loading: false,
+      },
+    };
+  },
+  watch: {
+    profile_image_url() {
+      // called when the select file has changed then reset the error
+      this.profile_image.error = "";
+      if (this.profile_image.file) this.change_icon_dialog = true;
+    },
+  },
+  computed: {
+    profile_image_url() {
+      if (this.profile_image.file)
+        return URL.createObjectURL(this.profile_image.file);
+      return this.my_profile.image;
+    },
+  },
+  methods: {
+    on_profile_img_load_err() {
+      this.show_dummy_img = true;
+    },
+    on_profile_image_reject() {
+      this.profile_image.error = "Select an image";
+    },
+    on_change_icon() {
+      this.$refs.file_select.$el.click();
+    },
+    save_profile_image() {
+      this.profile_image.loading = true;
+      this.$refs.cropper.getCroppedCanvas().toBlob((blob) => {
+        this.$store
+          .dispatch(PROFILE_IMAGE_UPDATE, blob)
+          .then(() => {
+            this.profile_image.loading = false;
+            this.profile_image.file = null;
+            this.change_icon_dialog = false;
+          })
+          .catch((error) => {
+            this.profile_image.loading = false;
+            // TODO: show error message wither on the popup or seperately
+            this.change_icon_dialog = false;
+            console.log(error);
+          });
+      }, "image/png");
+    },
+  },
+};
+</script>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,89 +1,31 @@
 <template>
   <base-layout>
-    <div class="q-pa-md">
+    <div class="q-pa-md" v-if="hidden_mode">
       <div class="row justify-center">
-        <div class="col text-center">
-          <div class="row justify-center">
-            <q-avatar
-              size="110px"
-              style="margin-left: 8px"
-              :class="{ 'bg-grey-9': show_dummy_img || !my_profile.image }"
-            >
-              <img
-                :src="my_profile.image"
-                @error="on_profile_img_load_err"
-                v-if="!show_dummy_img && my_profile.image"
-              />
-              <q-icon name="mdi-account" size="145px" color="grey-5" v-else />
-            </q-avatar>
-            <div class="self-end" style="margin-left: -24px">
-              <q-btn
-                round
-                flat
-                @click="on_change_icon"
-                size="8px"
-                color="white"
-                v-ripple
-                class="q-pa-xs bg-grey-7"
-                icon="mdi-camera"
-              />
-            </div>
-          </div>
-          <div class="text-h5 text-weight-bold q-mt-md">
-            {{ my_profile.name }}
-          </div>
+        <q-skeleton class="q-mx-sm" type="QAvatar" size="100px" />
+      </div>
+      <div class="row q-mt-md justify-center">
+        <div class="col-8 text-center">
+          <q-skeleton type="rect" />
         </div>
       </div>
       <div class="row q-mt-lg">
         <div class="col-8 offset-2">
           <div class="row">
             <div class="col-4 text-center">
-              <q-skeleton class="q-mx-sm" type="text" v-if="hide_mode" />
-              <div class="text-uppercase text-title text-weight-bolder" v-else>
-                cinephile
-              </div>
-              <div class="q-mt-xs text-uppercase text-caption">level</div>
+              <q-skeleton class="q-mx-sm" type="text" />
             </div>
             <div class="col-4 text-center">
-              <q-skeleton class="q-mx-sm" type="text" v-if="hide_mode" />
-              <div class="text-uppercase text-title text-weight-bolder" v-else>
-                100
-              </div>
-              <div class="q-mt-xs text-uppercase text-caption">rank</div>
+              <q-skeleton class="q-mx-sm" type="text" />
             </div>
             <div class="col-4 text-center">
-              <q-skeleton class="q-mx-sm" type="text" v-if="hide_mode" />
-              <div class="text-uppercase text-title text-weight-bolder" v-else>
-                300
-              </div>
-              <div class="q-mt-xs text-uppercase text-caption">reviews</div>
+              <q-skeleton class="q-mx-sm" type="text" />
             </div>
           </div>
         </div>
       </div>
-      <q-skeleton class="q-mt-md q-mx-lg" type="text" v-if="hide_mode" />
-      <div class="row justify-center q-mt-md" v-else>
-        <q-linear-progress
-          size="5px"
-          :value="engagement"
-          track-color="white"
-          class="row q-mt-sm"
-          style="max-width: 180px"
-        />
-        <span class="text-xs q-ml-sm">
-          {{ engagement * 100 }} <q-icon name="mdi-percent" />
-          <q-icon name="mdi-chevron-down"
-        /></span>
-      </div>
-      <div class="row justify-around q-mt-none q-pa-none">
-        <div
-          class="text-overline text-uppercase"
-          style="font-size: 9px; line-height: 1em"
-        >
-          Engagement meter
-        </div>
-      </div>
-      <div class="q-mt-lg text-center" v-if="hide_mode">
+      <q-skeleton class="q-mt-md q-mx-lg" type="text" />
+      <div class="q-mt-lg text-center">
         <div>Sign In to check your profile</div>
         <q-btn
           flat
@@ -93,281 +35,35 @@
           label="Sign In"
         />
       </div>
-      <div class="q-mt-md" v-else>
-        <q-card flat>
-          <q-tabs
-            v-model="tab"
-            dense
-            class="white"
-            indicator-color="primary"
-            align="justify"
-            inline-label
-            outside-arrows
-            mobile-arrows
-          >
-            <q-tab name="watchlist" label="Watchlist" />
-            <q-tab name="recommends" label="Recommends" />
-            <q-tab name="lists" label="Lists" />
-            <q-tab name="followers" :label="followers.length + ' Followers'" />
-            <q-tab name="following" :label="followings.length + ' Following'" />
-          </q-tabs>
-          <q-separator />
-          <q-tab-panels v-model="tab" animated>
-            <q-tab-panel name="watchlist" class="q-px-none">
-              <movie-list
-                :source="watchlist"
-                @item-selected="on_movie_click"
-              ></movie-list>
-            </q-tab-panel>
-            <q-tab-panel name="recommends">
-              <movie-list
-                :source="recommends"
-                @item-selected="on_movie_click"
-              ></movie-list>
-            </q-tab-panel>
-            <q-tab-panel name="lists">
-              <q-list>
-                <q-item
-                  v-for="list in my_lists"
-                  :key="list.id"
-                  class="q-ma-none"
-                  v-ripple
-                  clickable
-                  @click="on_list_click(list)"
-                >
-                  <q-item-section>
-                    <q-item-label class="text-title">
-                      {{ list.name }}
-                    </q-item-label>
-                  </q-item-section>
-                  <q-item-section side>
-                    <q-item-label class="text-center">
-                      <div class="text-title">{{ list.movies.length }}</div>
-                      <div
-                        class="text-caption text-uppercase"
-                        style="font-size: 0.7em"
-                      >
-                        Movies
-                      </div>
-                    </q-item-label>
-                  </q-item-section>
-                  <q-item-section side>
-                    <q-item-label class="text-center">
-                      <div class="text-title">{{ list.like_count }}</div>
-                      <div
-                        class="text-caption text-uppercase"
-                        style="font-size: 0.7em"
-                      >
-                        Likes
-                      </div>
-                    </q-item-label>
-                  </q-item-section>
-                  <q-item-section side>
-                    <q-btn
-                      size="sm"
-                      flat
-                      round
-                      icon="mdi-dots-vertical"
-                      @click.stop="list_item_menu = !list_item_menu"
-                    >
-                    </q-btn>
-                  </q-item-section>
-                </q-item>
-              </q-list>
-            </q-tab-panel>
-            <q-tab-panel name="following"> </q-tab-panel>
-            <q-tab-panel name="follows"> </q-tab-panel>
-          </q-tab-panels>
-        </q-card>
-      </div>
-      <q-dialog v-model="list_item_menu">
-        <q-card class="" style="width: 400px; max-width: 50vw">
-          <q-card-section>
-            <q-list class="">
-              <q-item clickable v-ripple v-close-popup>
-                <q-item-section side>
-                  <q-icon name="mdi-border-color" size="xs" />
-                </q-item-section>
-                <q-item-section> Edit </q-item-section>
-              </q-item>
-              <q-item clickable v-ripple v-close-popup>
-                <q-item-section side>
-                  <q-icon name="mdi-trash-can" size="xs" />
-                </q-item-section>
-                <q-item-section> Delete </q-item-section>
-              </q-item>
-            </q-list>
-          </q-card-section>
-        </q-card>
-      </q-dialog>
-      <q-dialog v-model="change_icon_dialog">
-        <q-card class="" style="width: 400px; max-width: 80vw">
-          <q-card-section class="text-center">
-            <div class="text-h6 q-mb-lg">Change Picture</div>
-            <div>
-              <vue-cropper
-                ref="cropper"
-                :aspect-ratio="1"
-                :src="profile_image_url"
-                alt="Profile Picture"
-              >
-              </vue-cropper>
-            </div>
-          </q-card-section>
-          <q-card-actions align="right">
-            <q-btn flat label="Cancel" v-close-popup />
-            <q-btn
-              flat
-              :loading="profile_image.loading"
-              :disable="!profile_image.file"
-              color="primary"
-              label="Upload"
-              @click="save_profile_image"
-            />
-          </q-card-actions>
-        </q-card>
-      </q-dialog>
-      <div style="display: none">
-        <q-file
-          ref="file_select"
-          v-model="profile_image.file"
-          accept=".jpg, image/*"
-          @rejected="on_profile_image_reject"
-        />
-      </div>
     </div>
+    <template v-else>
+      <profile-audience v-if="is_audience_profile"></profile-audience>
+      <profile-filmmaker v-else></profile-filmmaker>
+    </template>
   </base-layout>
 </template>
 <script>
-import VueCropper from "vue-cropperjs";
-import "cropperjs/dist/cropper.css";
 import BaseLayout from "@/layouts/Base";
-import MovieList from "@/components/MovieList";
-import {
-  PROFILE_WATCHLIST_REQUEST,
-  PROFILE_RECOMMENDS_REQUEST,
-  PROFILE_IMAGE_UPDATE,
-} from "@/store/actions";
-import { mapState } from "vuex";
+import ProfileAudience from "@/components/ProfileAudience";
+import ProfileFilmmaker from "@/components/ProfileFilmmaker";
 export default {
   name: "profile-page",
   components: {
     BaseLayout,
-    MovieList,
-    VueCropper,
+    ProfileAudience,
+    ProfileFilmmaker,
   },
   metaInfo: {
     title: "Profile",
   },
   data() {
     return {
-      show_dummy_img: false,
-      tab: "watchlist",
-      engagement: 0.86,
-      followers: [],
-      followings: [],
-      profile_image: {
-        file: null,
-        error: "",
-        loading: false,
-      },
-      new_profile_image: null,
-      selected_file_error: "",
-      xp_info_dialog: false,
-      earning_info_dialog: false,
-      badge_info_dialog: false,
-      edit_name_dialog: false,
-      change_icon_dialog: false,
-      list_item_menu: false,
+      is_audience_profile: true,
     };
   },
   computed: {
-    ...mapState("profile", ["watchlist", "recommends"]),
-    ...mapState("list", ["my_lists"]),
-    show_login_popup() {
+    hidden_mode() {
       return !this.is_authenticated;
-    },
-    hide_mode() {
-      return !this.is_authenticated;
-    },
-    profile_image_url() {
-      if (this.profile_image.file)
-        return URL.createObjectURL(this.profile_image.file);
-      return this.my_profile.image;
-    },
-  },
-  watch: {
-    profile_image_url() {
-      // called when the select file has changed then reset the error
-      this.profile_image.error = "";
-      this.change_icon_dialog = true;
-    },
-  },
-  mounted() {
-    if (this.is_authenticated) {
-      this.$store.dispatch(PROFILE_WATCHLIST_REQUEST);
-      this.$store.dispatch(PROFILE_RECOMMENDS_REQUEST);
-    }
-  },
-  methods: {
-    show_xp_info_dialog() {
-      this.xp_info_dialog = true;
-    },
-    show_earning_info_dialog() {
-      this.earning_info_dialog = true;
-    },
-    show_badge_info_dialog() {
-      this.badge_info_dialog = true;
-    },
-    show_edit_popup() {
-      this.edit_name_dialog = true;
-    },
-    on_movie_click(item) {
-      this.$router.push({
-        name: "movie-detail",
-        params: { id: item.id, slug: this.slugify(item.title) },
-      });
-    },
-    on_list_click(list) {
-      // redirect to page where he can see his list stats and movies in it
-      console.log(list);
-    },
-    on_change_icon() {
-      this.$refs.file_select.$el.click();
-    },
-    on_profile_img_load_err() {
-      console.log("image load failed");
-      this.show_dummy_img = true;
-    },
-    list_description(list) {
-      var plural = list.movies.length == 0 || list.movies.length > 1 ? "s" : "";
-      var prefix = list.movies.length == 0 ? "No" : list.movies.length;
-      return `${prefix} Movie${plural}`;
-    },
-    list_like_text(list) {
-      var plural = list.like_count == 0 || list.like_count > 1 ? "s" : "";
-      var prefix = list.like_count == 0 ? "No" : list.like_count;
-      return `${prefix} like${plural}`;
-    },
-    on_profile_image_reject() {
-      this.profile_image.error = "Select an image";
-    },
-    save_profile_image() {
-      this.profile_image.loading = true;
-      this.$refs.cropper.getCroppedCanvas().toBlob((blob) => {
-        this.$store
-          .dispatch(PROFILE_IMAGE_UPDATE, blob)
-          .then(() => {
-            this.profile_image.loading = false;
-            this.change_icon_dialog = false;
-          })
-          .catch((error) => {
-            this.profile_image.loading = false;
-            // TODO: show error message wither on the popup or seperately
-            this.change_icon_dialog = false;
-            console.log(error);
-          });
-      }, "image/png");
     },
   },
 };
