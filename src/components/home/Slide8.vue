0d92afe3dc98d3b3977e9abe68262a02d07c8355
Zeeshan Khan <zkhan1093@gmail.com>
2020-12-10 22:54:35 +0530

    removed unused assets and resized some

--- src/components/home/Slide8.vue
+++ src/components/home/Slide8.vue
@@ -20,7 +20,7 @@
         <bar-plus :rtl="true" />
       </div>
       <div class="col-12 text-center q-mt-lg" style="padding-bottom: 0px">
-        <img src="/img/8.png" width="100%" style="max-width: 300px" />
+        <img src="/img/8.jpg" width="100%" style="max-width: 300px" />
       </div>
     </div>
   </div>
