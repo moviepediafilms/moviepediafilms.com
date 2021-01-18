6ea7eba040f7abe1bfb329f8c60461e5db6c8a00
Zeeshan Khan <zkhan1093@gmail.com>
2021-01-19 00:13:43 +0530

    adding not found UI on wrong profile URL

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,25 +1,14 @@
 <template>
   <base-layout>
-    <div class="q-my-lg q-py-lg text-center">
-      <div class="text-h1 text-weight-thin" style="letter-spacing: 0.15em">
-        404
-      </div>
-      <div class="text-overline text-weight-light">It seems you are lost!</div>
-      <div class="q-mt-lg">
-        <router-link
-          :to="{ name: 'home' }"
-          class="text-primary text-decoration-none"
-          >Home</router-link
-        >
-      </div>
-    </div>
+    <not-found />
   </base-layout>
 </template>
 <script>
+import NotFound from "@/components/NotFound";
 import BaseLayout from "@/layouts/Base";
 export default {
-  name: "not-found",
-  components: { BaseLayout },
+  name: "not-found-page",
+  components: { BaseLayout, NotFound },
   data() {
     return {};
   },
