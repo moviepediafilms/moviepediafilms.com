0df9fbaf8072789deaa00a191f152dbb1f6f003b
Zeeshan Khan <zkhan1093@gmail.com>
2021-01-23 00:11:16 +0530

    removing transition group

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -1,90 +1,81 @@
 <template>
   <div>
-    <transition
-      appear
-      name="custom-classes-transition"
-      enter-active-class="animated animate__fadeIn"
-      leave-active-class="animated animate__fadeOut"
-      mode="out-in"
-      :duration="200"
-    >
-      <q-list padding ref="list" v-if="users.length > 0">
-        <q-item dense class="q-ma-none q-pa-none">
-          <q-item-section avatar> </q-item-section>
-          <q-item-section class="q-ml-lg text-caption text-left text-grey-5">
-          </q-item-section>
-          <q-item-section
-            side
-            class="text-caption text-grey-5"
-            style="min-width: 50px"
-          >
-            Points
-          </q-item-section>
-          <q-item-section
-            side
-            class="text-caption text-grey-5"
-            style="min-width: 50px"
-          >
-            #Rank
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          v-ripple
-          class="bg-light-green-10"
-          style="border-radius: 10px"
-          v-if="pin_self_top"
+    <q-list padding ref="list" v-if="users.length > 0">
+      <q-item dense class="q-ma-none q-pa-none">
+        <q-item-section avatar> </q-item-section>
+        <q-item-section class="q-ml-lg text-caption text-left text-grey-5">
+        </q-item-section>
+        <q-item-section
+          side
+          class="text-caption text-grey-5"
+          style="min-width: 50px"
         >
-          <q-item-section avatar>
-            <q-avatar>
-              <img :src="my_profile.image" @error="on_user_img_error" />
-            </q-avatar>
-          </q-item-section>
-          <q-item-section no-wrap class="ellipsis text-left">
-            {{ my_profile.name }}
-            <br />
-            <span class="text-caption">{{ my_profile.city }}</span>
-          </q-item-section>
-          <q-item-section side style="min-width: 55px">
-            {{ my_profile[pointFrom] }}
-          </q-item-section>
-          <q-item-section side style="min-width: 55px">
-            #{{ my_profile[rankFrom] }}
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          v-ripple
-          v-for="(user, index) in users"
-          :key="user.id"
-          style="border-radius: 10px"
-          :class="{ 'bg-light-green-10 q-mb-xs': index < highlight_top }"
-          @click="$emit('click', user)"
+          Points
+        </q-item-section>
+        <q-item-section
+          side
+          class="text-caption text-grey-5"
+          style="min-width: 50px"
         >
-          <q-item-section avatar>
-            <q-avatar>
-              <img
-                :src="user.image || '/default_avatar.png'"
-                @error="on_user_img_error"
-              />
-            </q-avatar>
-          </q-item-section>
-          <q-item-section no-wrap class="ellipsis text-left">
-            {{ user.name }}
-            <span class="text-grey-6 text-caption">{{ user.city }}</span>
-          </q-item-section>
-          <q-item-section side style="min-width: 55px">
-            {{ user[pointFrom] }}
-          </q-item-section>
-          <q-item-section side style="min-width: 55px">
-            {{ user[rankFrom] }}
-          </q-item-section>
-        </q-item>
-      </q-list>
-      <div class="text-center q-my-lg" v-if="loading">
-        <q-spinner-hourglass color="grey-6" size="2em" />
-      </div>
-    </transition>
+          #Rank
+        </q-item-section>
+      </q-item>
+      <q-item
+        clickable
+        v-ripple
+        class="bg-light-green-10"
+        style="border-radius: 10px"
+        v-if="pin_self_top"
+      >
+        <q-item-section avatar>
+          <q-avatar>
+            <img :src="my_profile.image" @error="on_user_img_error" />
+          </q-avatar>
+        </q-item-section>
+        <q-item-section no-wrap class="ellipsis text-left">
+          {{ my_profile.name }}
+          <br />
+          <span class="text-caption">{{ my_profile.city }}</span>
+        </q-item-section>
+        <q-item-section side style="min-width: 55px">
+          {{ my_profile[pointFrom] }}
+        </q-item-section>
+        <q-item-section side style="min-width: 55px">
+          #{{ my_profile[rankFrom] }}
+        </q-item-section>
+      </q-item>
+      <q-item
+        clickable
+        v-ripple
+        v-for="(user, index) in users"
+        :key="user.id"
+        style="border-radius: 10px"
+        :class="{ 'bg-light-green-10 q-mb-xs': index < highlight_top }"
+        @click="$emit('click', user)"
+      >
+        <q-item-section avatar>
+          <q-avatar>
+            <img
+              :src="user.image || '/default_avatar.png'"
+              @error="on_user_img_error"
+            />
+          </q-avatar>
+        </q-item-section>
+        <q-item-section no-wrap class="ellipsis text-left">
+          {{ user.name }}
+          <span class="text-grey-6 text-caption">{{ user.city }}</span>
+        </q-item-section>
+        <q-item-section side style="min-width: 55px">
+          {{ user[pointFrom] }}
+        </q-item-section>
+        <q-item-section side style="min-width: 55px">
+          {{ user[rankFrom] }}
+        </q-item-section>
+      </q-item>
+    </q-list>
+    <div class="text-center q-my-lg" v-if="loading">
+      <q-spinner-hourglass color="grey-6" size="2em" />
+    </div>
     <div class="row justify-center q-mt-md" v-if="show_page_indicator">
       <q-pagination
         v-model="curr_page"
