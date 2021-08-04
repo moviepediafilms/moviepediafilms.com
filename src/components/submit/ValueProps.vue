b1f6546069cc612626c75584b6c1f866a25f2c8a
Zeeshan Khan <zkhan1093@gmail.com>
2021-08-04 07:50:48 +0530

    mdff2 changes

--- src/components/submit/ValueProps.vue
+++ src/components/submit/ValueProps.vue
@@ -16,9 +16,9 @@
     </q-list>
     <div class="text-body1 q-my-sm text-grey-5">
       <div class="text-title text-grey-4">
-        {{ rs }} 399 per Standard Submision
+        MDFF : {{ rs }} 499 per submission
       </div>
-      <div class="q-mt-sm text-body2">
+      <div class="q-mt-sm text-body2 hidden">
         (if this one doesn't get featured, your next film submission is on us)
       </div>
     </div>
@@ -33,39 +33,33 @@ export default {
         {
           icon: "mdi-account-group",
           color: "light-green",
-          text:
-            "We provide you the platform to set up your own OTT profile, supporting fundraising to screening of your films.",
+          text: "We provide you the platform to set up your own OTT profile, supporting fundraising to screening of your films.",
         },
-        {
-          icon: "mdi-youtube",
-          color: "red-8",
-          text:
-            "Build a consistent audience for your YouTube releases. We drive a traffic of over 160,000 cinephiles to your content.",
-        },
+        // {
+        //   icon: "mdi-youtube",
+        //   color: "red-8",
+        //   text: "Build a consistent audience for your YouTube releases. We drive a traffic of over 160,000 cinephiles to your content.",
+        // },
         {
           icon: "mdi-account-star",
           color: "blue",
-          text:
-            "Your film reaches the network of our ‘Partner Celebs’ and stands a chance to get recommended across our network.",
+          text: "Your film reaches the network of our ‘Partner Celebs’ and stands a chance to get recommended across our network.",
         },
         {
           icon: "mdi-trophy",
           color: "amber",
-          text:
-            "You stand a chance to win the ‘Creator of the Month’ title, cash prize of over Rs.15,000 and an extended feature period on the platform.",
+          text: "You stand a chance to win cash rewards worth Rs.15,000",
         },
         {
           icon: "mdi-bullseye-arrow",
           color: "deep-purple",
-          text:
-            "We curate your profile as a filmmaker and connect you with the audience. We strive to manage your reputation as a filmmaker.",
+          text: "We curate your profile as a filmmaker and connect you with the audience. We strive to manage your reputation as a filmmaker.",
         },
-        {
-          icon: "mdi-check-decagram",
-          color: "light-blue",
-          text:
-            "We simplify the fundraising for your projects. We connect you with film enthusiasts to get your projects funded.",
-        },
+        // {
+        //   icon: "mdi-check-decagram",
+        //   color: "light-blue",
+        //   text: "We simplify the fundraising for your projects. We connect you with film enthusiasts to get your projects funded.",
+        // },
       ],
     };
   },
