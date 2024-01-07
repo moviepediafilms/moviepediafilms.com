878b270afc95eafa99ee535c775fb416c1b365a5
Zeeshan Khan <zkhan1093@gmail.com>
2024-01-06 23:56:10 -0500

    fix my submissions error

--- src/src/views/MySubmissions.vue
+++ src/src/views/MySubmissions.vue
@@ -10,13 +10,8 @@
           <div class="col-12" v-for="movie in submissions" :key="movie.id">
             <div class="row">
               <q-card class="col-3 q-ma-md">
-                <movie-image
-                  class="col-4"
-                  :title="movie.title"
-                  :state="movie.state"
-                  :show-state="true"
-                  :poster="movie.poster"
-                />
+                <movie-image class="col-4" :title="movie.title" :state="movie.state" :show-state="true"
+                  :poster="movie.poster" />
               </q-card>
               <div class="col">
                 <div>
@@ -24,11 +19,7 @@
                     {{ movie.title }}
                   </div>
 
-                  <div
-                    class="q-mt-lg"
-                    :key="order.id"
-                    v-for="order in movie.orders"
-                  >
+                  <div class="q-mt-lg" :key="order.id" v-for="order in movie.orders">
                     <div class="q-mt-xs" v-if="order.package">
                       <div class="text-h4 text-primary">
                         {{ package_id_to_obj[order.package].name }}
@@ -47,36 +38,18 @@
                       {{ order.payment_id }}
                     </div>
 
-                    <q-btn
-                      size="sm"
-                      label="Pay"
-                      color="primary"
-                      class="q-mt-xs"
-                      text-color="dark"
+                    <q-btn size="sm" label="Pay" color="primary" class="q-mt-xs" text-color="dark"
                       :to="{ name: 'submit', params: { movie_id: movie.id } }"
-                      v-if="has_active_package(order) && is_not_paid(order)"
-                    />
-                    <q-btn
-                      size="sm"
-                      class="q-mt-md"
-                      label="Select Package"
-                      color="primary"
-                      text-color="dark"
-                      :to="{ name: 'submit', params: { movie_id: movie.id } }"
-                      v-if="!order.order_id"
-                    />
+                      v-if="has_active_package(order) && is_not_paid(order)" />
+                    <q-btn size="sm" class="q-mt-md" label="Select Package" color="primary" text-color="dark"
+                      :to="{ name: 'submit', params: { movie_id: movie.id } }" v-if="!order.order_id" />
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <empty-state
-          title="Nothing here to see"
-          desc="Submit movie now!"
-          icon="mdi-emoticon-sad"
-          v-else
-        />
+        <empty-state title="Nothing here to see" desc="Submit movie now!" icon="mdi-emoticon-sad" v-else />
       </div>
     </div>
   </base-layout>
@@ -115,6 +88,12 @@ export default {
   },
   methods: {
     has_active_package(order) {
+      if (!order.package) {
+        return false;
+      }
+      if (!this.package_id_to_obj[order.package]) {
+        return false;
+      }
       return this.package_id_to_obj[order.package].active;
     },
     is_not_paid(order) {
